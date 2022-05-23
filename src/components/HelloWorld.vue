f5c358b67979bb568601fe27abb166c00293be2c
mahdi221b <mahdi221b221m@gmail.com>
2022-05-23 14:25:28 -0700

    اضافه کردن کامنت به بخش template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,40 +3,40 @@
 
 <template>
   <div>
-    <table id="calculator" dir="rtl">
+    <table id="calculator" dir="rtl">  <!--جدول-->
       <tr>
       
         <td colspan="4">
-          <input v-model="screen" dir="rtl" id="show" type="number"/>
+          <input v-model="screen" dir="rtl" id="show" type="number"/>   <!--صفحه نمایش-->
         </td>
       
       </tr>
       <tr>
-        <td><button @click="mod" id="mod">%</button></td>
-        <td colspan="2"><button @click="clear" id="clear">C</button></td>
-        <td><button @click="mul" id="mul">*</button></td>
+        <td><button @click="mod" id="mod">%</button></td>   <!--دکمه‌ی درصد-->
+        <td colspan="2"><button @click="clear" id="clear">C</button></td>    <!--دکمه‌ی پاک کردن صفحه نمایش-->
+        <td><button @click="mul" id="mul">*</button></td>   <!--دکمه‌ی ضربدر-->
       </tr>
       <tr>
-        <td><button @click="digit(9)" id="digit" class="digit">9</button></td>
-        <td><button @click="digit(8)" id="digit" class="digit">8</button></td>
-        <td><button @click="digit(7)" id="digit" class="digit">7</button></td>
-        <td><button @click="div" id="div">/</button></td>
+        <td><button @click="digit(9)" id="digit" class="digit">9</button></td>   <!--9دکمه‌ی عدد-->
+        <td><button @click="digit(8)" id="digit" class="digit">8</button></td>   <!--8دکمه‌ی عدد-->
+        <td><button @click="digit(7)" id="digit" class="digit">7</button></td>   <!--7دکمه‌ی عدد-->
+        <td><button @click="div" id="div">/</button></td>   <!--دکمه‌ی تقسیم-->
       </tr>
       <tr>
-        <td><button @click="digit(6)" id="digit" class="digit">6</button></td>
-        <td><button @click="digit(5)" id="digit" class="digit">5</button></td>
-        <td><button @click="digit(4)" id="digit" class="digit">4</button></td>
-        <td><button @click="minus" id="minus">-</button></td>
+        <td><button @click="digit(6)" id="digit" class="digit">6</button></td>   <!--6دکمه‌ی عدد-->
+        <td><button @click="digit(5)" id="digit" class="digit">5</button></td>   <!--5دکمه‌ی عدد-->
+        <td><button @click="digit(4)" id="digit" class="digit">4</button></td>   <!--4دکمه‌ی عدد-->
+        <td><button @click="minus" id="minus">-</button></td>    <!--دکمه‌ی تفریق-->
       </tr>
       <tr>
-        <td><button @click="digit(3)" id="digit" class="digit">3</button></td>
-        <td><button @click="digit(2)" id="digit" class="digit">2</button></td>
-        <td><button @click="digit(1)" id="digit" class="digit">1</button></td>
-        <td rowspan="2" ><button @click="sum" id="sum">+</button></td> 
+        <td><button @click="digit(3)" id="digit" class="digit">3</button></td>   <!--3دکمه‌ی عدد-->
+        <td><button @click="digit(2)" id="digit" class="digit">2</button></td>   <!--2دکمه‌ی عدد-->
+        <td><button @click="digit(1)" id="digit" class="digit">1</button></td>   <!--1دکمه‌ی عدد-->
+        <td rowspan="2" ><button @click="sum" id="sum">+</button></td>    <!--دکمه‌ی جمع-->
       </tr>
       <tr>
-        <td colspan="2"><button @click="equal" id="equal">=</button></td> 
-        <td><button @click="digit(0)" id="digit" class="digit">0</button></td>
+        <td colspan="2"><button @click="equal" id="equal">=</button></td>    <!--دکمه‌ی مساوی-->
+        <td><button @click="digit(0)" id="digit" class="digit">0</button></td>   <!--0دکمه‌ی عدد-->
       </tr>
     </table>
   </div>
